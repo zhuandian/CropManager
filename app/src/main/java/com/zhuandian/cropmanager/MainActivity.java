97d1f3c787a0919deaa2ba64169ad10c72e73300
xiedong <xiedong11@aliyun.com>
2019-03-12 19:25:38 +0800

    init project

---
+++ app/src/main/java/com/zhuandian/cropmanager/MainActivity.java
@@ -0,0 +1,16 @@
+package com.zhuandian.cropmanager;
+
+import com.zhuandian.cropmanager.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void setUpView() {
+
+    }
+}
