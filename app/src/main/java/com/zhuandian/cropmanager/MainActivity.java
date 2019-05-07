347404c18951fd2ae7074b9b568874d964b588e3
xiedong <xiedong11@aliyun.com>
2019-05-07 16:56:01 +0800

    修改客户端数据不能实时更新问题

--- app/src/main/java/com/zhuandian/cropmanager/MainActivity.java
+++ app/src/main/java/com/zhuandian/cropmanager/MainActivity.java
@@ -10,9 +10,15 @@ import android.widget.Toast;
 import com.zhuandian.cropmanager.base.BaseActivity;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -52,6 +58,7 @@ public class MainActivity extends BaseActivity {
     }
 
 
+
     @OnClick({R.id.tv_refresh_ph, R.id.tv_search})
     public void onClick(View view) {
         switch (view.getId()) {
@@ -74,33 +81,27 @@ public class MainActivity extends BaseActivity {
             //创建一个ServerSocket，port是客户端的端口
             ServerSocket serverSocket = new ServerSocket(9000);
             String s1 = serverSocket.getInetAddress().toString();
-            System.out.println("-------------------111-" + s1);
             while (true) {
                 //从请求队列中取出链接,如果只接受一次则不用使用while循环
                 Socket socket = serverSocket.accept();
                 //获取客户端信息
-                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                final String clientContent = input.readLine();
-                //接下来可以对信息进行操作，我这里是直接打印了，你可以读取或者做其他操作
-                System.out.println(clientContent + "-------------------");
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        tvPhLevel.setText(clientContent.split("=")[0]);
-//                        phLevel = clientContent
-                    }
-                });
+                InputStream inputStream = socket.getInputStream();
+                byte buffer[] = new byte[1024 * 4];
+                int temp = 0;
+                // 从InputStream当中读取客户端所发送的数据
+                while ((temp = inputStream.read(buffer)) != -1) {
+                    final String clientContent = new String(buffer, 0, temp);
+                    System.out.println("-----------"+clientContent);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            tvPhLevel.setText(clientContent.split("=")[1]);
+                        }
+                    });
+                }
+                serverSocket.close();
+                socket.close();
 
-                //初始化输出流，回复客户端
-                PrintStream out = new PrintStream(socket.getOutputStream());
-                //获取键盘输出的内容
-                String s = new BufferedReader(new InputStreamReader(System.in)).readLine();
-                //将信息发送给客户端
-                out.println("open");
-
-                //关闭
-                out.close();
-                input.close();
             }
         } catch (IOException e) {
             System.out.println("服务器异常:" + e);
